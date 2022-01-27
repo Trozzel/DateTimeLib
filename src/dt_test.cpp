818e5d90bfd60814f2f3a60e496751346ea5cf82
George Ford <george.ford@icloud.com>
2022-01-27 17:26:25 -0500

    added copy headers to /usr/local/include in CMake

--- src/dt_test.cpp
+++ src/dt_test.cpp
@@ -21,5 +21,9 @@ int main() {
 	cout << "t1: " << ct1 << endl;
 	cout << "t2: " << ct2 << endl;
 
+	cout << "I am adding two years to my birthday..." << endl;
+	ct1.addYears(2);
+	cout << "2 years later..." << ct1 << endl;
+
     return 0;
 }
